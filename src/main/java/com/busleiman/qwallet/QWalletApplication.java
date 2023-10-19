4aa7ab39f8054c5da6ec24f10ca7f272f93fce9d
franbusleiman <franbusleiman@gmail.com>
2023-10-19 10:28:04 -0300

    q-wallet initial commit

---
+++ src/main/java/com/busleiman/qwallet/QWalletApplication.java
@@ -0,0 +1,13 @@
+package com.busleiman.qwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/busleiman/qwallet/QWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busleiman.qwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
