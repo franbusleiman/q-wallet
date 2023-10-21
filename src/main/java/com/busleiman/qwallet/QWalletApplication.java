62acfcd0c4ddb3157eb62ac068ab2bd8b1692d36
franbusleiman <franbusleiman@gmail.com>
2023-10-20 21:47:34 -0300

    initial config for q-wallet added

--- src/main/java/com/busleiman/qwallet/QWalletApplication.java
+++ src/main/java/com/busleiman/qwallet/QWalletApplication.java
@@ -2,6 +2,9 @@ package com.busleiman.qwallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+@EnableR2dbcRepositories
 
 @SpringBootApplication
 public class QWalletApplication {
